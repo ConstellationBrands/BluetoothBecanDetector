c28ac02edc7ab079110d0766063e67656f49370a
saranya jayaseelan <saranyajayaseelan86@gmail.com>
2018-08-23 22:44:06 -0700

    Added Logger to track the beacon

---
+++ BluetoothBeaconDetector/Util/Logger.swift
@@ -0,0 +1,19 @@
+//
+//  Logger.swift
+//  BluetoothBeaconDetector
+//
+//  Created by Saranya Jayaseelan on 8/23/18.
+//  Copyright © 2018 Saranya Jayaseelan. All rights reserved.
+//
+
+import Foundation
+
+public class Logger : NSObject {
+    public var isDebugModeOn: Bool!
+    
+    public override init() {
+        self.isDebugModeOn = false
+    }
+
+}
+
